8f0885d0ff3b5c893d766f220aae438274dd3436
liuting <15122031512@139.com>
2019-05-07 17:18:02 +0800

    调整models、settings

--- happyPY/settings.py
+++ happyPY/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hlPY.apps.HlpyConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'happyPY.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learn',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -122,3 +127,5 @@ STATIC_URL = '/statics/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,  'hlPY/statics'),
 )
+
+AUTH_USER_MODEL = "hlPY.user_info"
--- hlPY/admin.py
+++ hlPY/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from .models import user_info
 # Register your models here.
+
