cfce409dc1525d490f1b90bde1c7e4801974986d
cecelia-liu <704343024@qq.com>
2019-05-13 16:11:58 +0800

    完成了登陆、注册页面，修改了models.py,完成编程环境功能及页面，完成用户信息和课程信息传递函数

--- happyPY/urls.py
+++ happyPY/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('login/',views.login,name='login'),
     path('register/',views.register,name='register'),
     path('ide/',views.ide,name='ide'),
-    path('user_page/',views.user_page,name='user_page'),
+    path('testcode/',views.testcode,name='testcode'),
+    path('idetest/',views.idetest,name='idetest'),
+    path('user_page/', views.user_page, name='user_page'),
 ]
--- hlPY/viewsall/login.py
+++ hlPY/viewsall/login.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 import datetime
 from hlPY.models import user_info
+from hlPY.viewsall import captcha
 
 
 '''登录函数，验证用户用户名和密码是否正确，如果正确，跳转到主页，如果不正确，则还停在登录页，并返回错误信息'''
@@ -41,5 +42,39 @@ def login(request):
     return render(request, 'login/login.html')
 
 
-
-
+'''后台验证验证码'''
+@csrf_exempt
+def login1(request):
+    #redirect_to = request.REQUEST.get('next', '')
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        identify = request.POST.get('identify', '')
+        captcha_text = captcha.get_captcha_text()
+        if identify == captcha_text:
+            '''
+               user = user_info.objects.get(username=username)
+               if user and user.password == password:
+                   auth.login(request, user)
+                   last_time = datetime.datetime.now()
+                   user_info.objects.filter(username=username).update(last_login=last_time)
+                   return HttpResponseRedirect("/")
+               '''
+            try:
+                user = authenticate(username=username, password=password)
+                if user and user.is_active:
+                    auth.login(request,user)
+                    '''从数据库中取出该用户的信息'''
+                    User = user_info.objects.get(username=username)
+                    '''将用户信息存入session中，为后续函数识别用户是否为登录状态做准备'''
+                    request.session['user'] = User
+                    last_time = datetime.datetime.now()
+                    user_info.objects.filter(username=username).update(last_login=last_time)
+                    return HttpResponseRedirect("/")
+                else:
+                    return render(request,"login/login.html",{"eror": "用户名或密码错误"})
+            except user_info.DoesNotExist:
+                pass
+        else:
+            return render(request, "login/login.html", {"eror": "验证码输入有误，请重新输入！"})
+    return render(request, 'login/login.html')
